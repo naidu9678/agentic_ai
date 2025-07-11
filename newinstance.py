bd7438ddaa2361ea3b93264c2269c1971535464d
Naidu <naidu9678@gmail.com>
2025-07-11 09:21:44 +0530

    Update newinstance.py

--- newinstance.py
+++ newinstance.py
@@ -4,8 +4,8 @@ import boto3
 #Function for connecting to EC2 
 ec2 = boto3.client('ec2',
 				'us-east-1',
-				aws_access_key_id='AKIAUYPNA4JPTKMDKU6J',
-				aws_secret_access_key='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')
+				aws_access_key_id='',
+				aws_secret_access_key='')
 
 #Function for running instances
 conn = ec2.run_instances(InstanceType="t2.micro",
